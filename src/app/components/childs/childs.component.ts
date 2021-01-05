4134952724a18630bc0a3660caa1a87f7a07ef9f
liquan1314 <lq1520299864@163.com>
2021-01-05 11:18:40 +0800

    修改后的的代码规范

--- src/app/components/childs/childs.component.ts
+++ src/app/components/childs/childs.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import * as shpjs from "shpjs";
 import {Latlng} from '../../classes/latlng';
 import {FormGroup,FormBuilder,FormControl} from '@angular/forms'
-import Polyline = google.maps.Polyline;
-// import shp from 'shpjs';
 
 @Component({
   selector: 'app-childs',
@@ -12,13 +9,9 @@ import Polyline = google.maps.Polyline;
 })
 export class ChildsComponent {
 
-  //对响应式的表单控件组进行配置数据
-  location = new FormGroup({
-      lat:new FormControl(''),
-      lng:new FormControl('')
-  })
 
-  //触发提交事件,得到用户输入的信息，然后改变中心点
+
+  //触发提交事件,得到用户输入的信息，然后改变中心点,事件处理器要加属性前缀on
   onSubmit () {
     const that = this;
     if(that.markers){
@@ -51,7 +44,9 @@ export class ChildsComponent {
 
   constructor() {
   }
-
+  //属性变量和方法，共有属性在前，私有属性在后，其次方法，注意事件处理器应该加上属性前缀on,与内置事件名一样
+  flightPaths: any; //多边形
+  infoWindow: any; //窗口对象
   //创建标记的数据
   locations = [
     {lat: -31.56391, lng: 147.154312,},
@@ -64,131 +59,56 @@ export class ChildsComponent {
     {lat: -36.817685, lng: 175.699196},
     {lat: -36.828611, lng: 175.790222},
   ];
-  uluru = {lat: -25.344, lng: 131.036};
+  //对响应式的表单控件组进行配置数据
+  location = new FormGroup({
+    lat:new FormControl(''),
+    lng:new FormControl('')
+  })
   map: any;  //创建地图
   markers: any; //创建标记
-  flightPaths: any; //多边形
-  infoWindow: any; //窗口对象
+  uluru = {lat: -25.344, lng: 131.036};
   uploadFile: any; //上传文件对象
-  //初始化地图
-  newMap(value) {
-    var divMap = document.querySelector('#map');
-    var that = this;
-    //创建一个地图对象
-    this.map = new google.maps.Map(document.querySelector('#map'), {
-      zoom: 5,  //这是显示地图的缩放，缩放的值越高，地图就越清晰
-      center: value,  //这是地图的中心点，让地图显示在我们选中的区域
-    })
-  }
-  //点击鼠标创建创建标记
-  newMarker() {
-    var that = this
-    that.markers = that.locations.map((location, i) => {
-      return new google.maps.Marker({
-        position: location,
-        map: that.map,
-        title: `${i}`,
-        label: `${i + 1}`
-      })
-    })
+  //data layer 处理geojson
+  convertToFeatures(data: any){
+    // Google map API is only support Feature and FeatureCollection.
+    let geoFeatures = [];
+
+    if (Array.isArray(data)) {
+      data.forEach((item) => {
+        geoFeatures = geoFeatures.concat(this.convertToFeatures(item));
+      });
+    } else {
+      //进行判断，判断geoJson数据里面的类型时features 还是 featuresCollection 还是GeometryCollection
+      switch (data.type) {
+        case 'FeatureCollection':
+          geoFeatures = data.features;
+          break;
+        case 'Feature':
+          geoFeatures.push(data);
+          break;
+        case 'GeometryCollection':    //返回一个
+          geoFeatures = data.geometries.map((item) => {
+            return {
+              type: 'Feature',
+              properties: {},
+              geometry: item, //几何类型
+            };
+          });
+          break;
+        default:
+          geoFeatures.push({
+            type: 'Feature',
+            properties: {},
+            geometry: data
+          });
+      }
+    }
+
+    return geoFeatures;
   }
 
-  //改变中心点
-  changeCenter(value) {
-    this.map.setCenter({
-      lat: value.lat,
-      lng: value.lng
-    })
-  }
-  //删除标记
-  deleteMarker() {
-    this.markers.map((item) => {
-      item.setMap(null)
-    })
-  }
-  //划线操作，根据我们得到的数据 Polyline绘制折线
-  painImg() {
-    var that = this;
-    const flightpath = new google.maps.Polyline({
-      path: that.locations, //这是路线的坐标点
-      geodesic: true,
-      strokeColor: 'yellow',
-      strokeOpacity: 0.5,
-      strokeWeight: 2
-    })
-    this.deleteMarker()
-    flightpath.setMap(that.map);
-  }
-  //绘制多边形的函数
-  painMoreImg() {
-    //flightpaths相当于是我们绘制了一个多边形，如果我们想在地图上展示它，我们就需要把它放到地图上
-    this.flightPaths = new google.maps.Polygon({
-      paths: this.locations, //这是指定多边形的路径
-      // strokeColor: '#f0f', //绘制多边形线条的颜色
-      // strokeOpacity: .6, //绘制多边形线条的透明度
-      // // strokeWeight: 2,  //绘制线条的厚度
-      // // fillColor: '#0ff', //填充颜色
-      // fillOpacity: .6, //填充颜色的透明度
-    })
-    this.flightPaths.setMap(this.map)
-    console.log(this.flightPaths.getPath().getAt(2).toString())
-  }
-
-  changeColor() {
-    this.flightPaths.setOptions({
-      fillColor: 'red'
-    });
-    console.log(1)
-    console.log(this.flightPaths.fillColor)
-  }
-
-  importData() {
-  }
-
-  //在地图上生成一个infoWindow
-  producetInfoWindow() {
-    alert(1)
-    let infoWindow = new google.maps.InfoWindow({
-      content: 'hello google'
-    })
-    const marker = new google.maps.Marker({
-      position: this.uluru,
-      map: this.map,
-    })
-    infoWindow.open(this.map, marker)
-  }
-  //导入数据生成的多边形
-  Ploygonpain(value){
-    let that = this;
-    //导入的数据，被转换为geoJSON在转换为坐标点，再到这的生成多边形 Ploygon
-    let ployGon = new google.maps.Polygon({
-      paths:value,
-      strokeColor:'red',
-      strokeOpacity:.6,
-      strokeWeight:2,
-      fillColor:'red',
-    })
-
-    ployGon.setMap(that.map)
-  }
-  // twoMap(value){
-  //   let map = new google.maps.Map(document.getElementById('app'),{
-  //     center:{lat: -25.344, lng: 131.036},
-  //     zoom:4,
-  //   })
-  //   let polygon = new google.maps.Polygon({
-  //       paths:value,
-  //       fillColor:'red',
-  //       strokeColor:'yellow'
-  //   })
-  //   polygon.setMap(map)
-  //   polygon.addListener('click',(event)=>{
-  //     console.log(event.latLng.lat(),event.latLng.lng())
-  //   })
-  // }
   //使用h5自带的方法 fileRead把shapfile解析成二进制，然后转为Geojson文件
   changShap() {
-    console.log(this)
     let that = this;
     let reader = new FileReader(); //实例化一个reader对象，对shapefile文件进行读取
     reader.readAsArrayBuffer(this.uploadFile.files[0]); //解析我上传的文件
@@ -249,18 +169,18 @@ export class ChildsComponent {
                   }
                   arr.push(arr1)
                 }
-                that.Ploygonpain(arr)
-              // }else if(coords[0].length !==1){
-              //   let MulityPolygon:any[] = [] //Polygon多变型的数组 一个feature就是一个数组 下面是对polygon的坐标解析
-              //   for (let j=0;j<coords.length;j++){
-              //     for(let h=0;h<coords[j].length;h++) {
-              //       let str = new Latlng(); //生成坐标点
-              //       str.lat = coords[j][h][1];
-              //       str.lng = coords[j][h][0];
-              //       MulityPolygon.push(str);
-              //     }
-              //   };
-              //   that.Ploygonpain(MulityPolygon)
+                that.painPloygon(arr)
+                // }else if(coords[0].length !==1){
+                //   let MulityPolygon:any[] = [] //Polygon多变型的数组 一个feature就是一个数组 下面是对polygon的坐标解析
+                //   for (let j=0;j<coords.length;j++){
+                //     for(let h=0;h<coords[j].length;h++) {
+                //       let str = new Latlng(); //生成坐标点
+                //       str.lat = coords[j][h][1];
+                //       str.lng = coords[j][h][0];
+                //       MulityPolygon.push(str);
+                //     }
+                //   };
+                //   that.Ploygonpain(MulityPolygon)
               }
             }
             else if(type === 1){
@@ -268,13 +188,13 @@ export class ChildsComponent {
               //解析Ploygon的数据
               let arrMulity:any[] = [] //Polygon多变型的数组 一个feature就是一个数组 下面是对polygon的坐标解析
               for (let j=0;j<coords.length;j++){
-                  for(let h=0;h<coords[j].length;h++) {
-                      let str = new Latlng(); //生成坐标点
-                      str.lat = coords[j][h][1];
-                      str.lng = coords[j][h][0];
-                      arrMulity.push(str);
-                  }
-                };
+                for(let h=0;h<coords[j].length;h++) {
+                  let str = new Latlng(); //生成坐标点
+                  str.lat = coords[j][h][1];
+                  str.lng = coords[j][h][0];
+                  arrMulity.push(str);
+                }
+              };
               // that.Ploygonpain(arrMulity)
             }
 
@@ -285,47 +205,126 @@ export class ChildsComponent {
     }
   }
 
+  //初始化地图
+  newMap(value) {
+    var divMap = document.querySelector('#map');
+    var that = this;
+    //创建一个地图对象
+    this.map = new google.maps.Map(document.querySelector('#map'), {
+      zoom: 5,  //这是显示地图的缩放，缩放的值越高，地图就越清晰
+      center: value,  //这是地图的中心点，让地图显示在我们选中的区域
+    })
+  }
   ngAfterViewInit(): void {
     this.newMap(this.uluru)
     this.uploadFile = document.getElementById('uploadFile');
   }
-
-  convertToFeatures(data: any){
-    // Google map API is only support Feature and FeatureCollection.
-    let geoFeatures = [];
-
-    if (Array.isArray(data)) {
-      data.forEach((item) => {
-        geoFeatures = geoFeatures.concat(this.convertToFeatures(item));
-      });
-    } else {
-      //进行判断，判断geoJson数据里面的类型时features 还是 featuresCollection 还是GeometryCollection
-      switch (data.type) {
-        case 'FeatureCollection':
-          geoFeatures = data.features;
-          break;
-        case 'Feature':
-          geoFeatures.push(data);
-          break;
-        case 'GeometryCollection':    //返回一个
-          geoFeatures = data.geometries.map((item) => {
-            return {
-              type: 'Feature',
-              properties: {},
-              geometry: item, //几何类型
-            };
-          });
-          break;
-        default:
-          geoFeatures.push({
-            type: 'Feature',
-            properties: {},
-            geometry: data
-          });
-      }
-    }
-
-    return geoFeatures;
+  //改变中心点
+  onChangeCenter(value) {
+    this.map.setCenter({
+      lat: value.lat,
+      lng: value.lng
+    })
   }
+
+  //改变多边形的颜色
+  onChangeColor() {
+    this.flightPaths.setOptions({
+      fillColor: 'red'
+    });
+  }
+
+  //删除标记
+  onDeleteMarker() {
+    this.markers.map((item) => {
+      item.setMap(null)
+    })
+  }
+
+  //点击鼠标创建创建标记
+  onNewMarker() {
+    var that = this
+    that.markers = that.locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        map: that.map,
+        title: `${i}`,
+        label: `${i + 1}`
+      })
+    })
+  }
+
+  //划线操作，根据我们得到的数据 Polyline绘制折线
+  onPainImg() {
+    var that = this;
+    const flightpath = new google.maps.Polyline({
+      path: that.locations, //这是路线的坐标点
+      geodesic: true,
+      strokeColor: 'yellow',
+      strokeOpacity: 0.5,
+      strokeWeight: 2
+    })
+    this.onDeleteMarker()
+    flightpath.setMap(that.map);
+  }
+  //绘制多边形的函数
+  onPainMoreImg() {
+    //flightpaths相当于是我们绘制了一个多边形，如果我们想在地图上展示它，我们就需要把它放到地图上
+    this.flightPaths = new google.maps.Polygon({
+      paths: this.locations, //这是指定多边形的路径
+      strokeColor: '#f0f', //绘制多边形线条的颜色
+      strokeOpacity: .6, //绘制多边形线条的透明度
+      strokeWeight: 2,  //绘制线条的厚度
+      fillColor: '#0ff', //填充颜色
+      fillOpacity: .6, //填充颜色的透明度
+    })
+    this.flightPaths.setMap(this.map)
+  }
+
+  //在地图上生成一个infoWindow
+  onProductInfoWindow() {
+    alert(1)
+    let infoWindow = new google.maps.InfoWindow({
+      content: 'hello google'
+    })
+    const marker = new google.maps.Marker({
+      position: this.uluru,
+      map: this.map,
+    })
+    infoWindow.open(this.map, marker)
+  }
+
+  //导入数据生成的多边形 函数一般使用小驼峰命名法
+  painPloygon(value){
+    let that = this;
+    //导入的数据，被转换为geoJSON在转换为坐标点，再到这的生成多边形 Ploygon
+    let ployGon = new google.maps.Polygon({
+      paths:value,
+      strokeColor:'red',
+      strokeOpacity:.6,
+      strokeWeight:2,
+      fillColor:'red',
+    })
+    ployGon.setMap(that.map)
+  }
+  // twoMap(value){
+  //   let map = new google.maps.Map(document.getElementById('app'),{
+  //     center:{lat: -25.344, lng: 131.036},
+  //     zoom:4,
+  //   })
+  //   let polygon = new google.maps.Polygon({
+  //       paths:value,
+  //       fillColor:'red',
+  //       strokeColor:'yellow'
+  //   })
+  //   polygon.setMap(map)
+  //   polygon.addListener('click',(event)=>{
+  //     console.log(event.latLng.lat(),event.latLng.lng())
+  //   })
+  // }
+
+
+
+
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,9 +8,10 @@ import {NavigationExtras,Router} from '@angular/router'
 export class HomeComponent{
 
   constructor(public router:Router) { }
-  //跳转的方法
-  Navigate():void{
-    let queryparams:NavigationExtras={
+  //跳转的方法  方法名一般使用小驼峰命名法
+  navigate():void{
+    //变量名如果没有被重新赋值的话，我们最好使用const 而不是let
+    const queryparams:NavigationExtras = {
       queryParams:{
           name:'home组件跳转过来的'
       }
